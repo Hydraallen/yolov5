2943fe4a52c963fc87c6e07354ca0a36d13b4678
Hydraallen <wangruiallen@gmail.com>
2024-02-07 13:59:23 +0800

    add: dataset

---
+++ DataSets/README.md
@@ -0,0 +1,46 @@
+# README
+
+[配置参考](https://www.modb.pro/db/605993)
+
+## Usage
+
+`train.py` 
+
+weights：权重文件路径
+
+cfg：存储模型结构的配置文件
+
+data：存储训练、测试数据的文件
+
+epochs：指的就是训练过程中整个数据集将被迭代（训练）了多少次，轮数，默认300，需要指定；说明一下基本上epochs小于100时训练出来的模型没有任何参考意义。
+
+batch-size：单次吞吐的数据多少，基本上就是电脑的运存，建议为80%左右电脑内存（16g运存就分配个12g那样）
+
+resume： 重新训练默认是false，但是当你的训练因为一些因素被迫暂停了，可以将其置为True来检查断点（checkpoint）继续训练；
+
+img-size：输入图片宽高；
+
+device：cuda device, i.e. 0 or 0,1,2,3 or cpu。选择使用GPU还是CPU
+
+workers：线程数。默认是8。现在大家的电脑CPU普遍是8核心起步，所以CPU训练时候不会报错，但是当你选择GPU训练时，显卡一般都是流处理器，所以我们要将其设为1，或者是0，否则会爆出内存不够的BUG。
+
+`models` 存放已有模型参数
+
+`runs`存放模型训练结果
+
+`DataSets`存放训练集
+
+`DataSets_apart.py` 拆分数据集
+
+`DataSets_tranXML.py`生成`.txt`文件
+
+## Test
+
+```bash
+python DataSets_apart.py
+python DataSets_tranXML.py
+python DataSetsTrain.py
+```
+
+
+
---
+++ DataSets_apart.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+# 划分数据集
+import os
+import random
+import argparse
+
+parser = argparse.ArgumentParser()
+# xml文件的地址，根据自己的数据进行修改 xml一般存放在Annotations下
+parser.add_argument('--xml_path', default='./DataSets/annotations', type=str, help='xml path')
+# xml文件的地址，根据自己的数据进行修改 xml一般存放在Annotations下
+parser.add_argument('--img_path', default='./DataSets/images', type=str, help='img path')
+# 数据集划分后txt文件的存储地址，地址选择自己数据下的ImageSets/Main
+parser.add_argument('--txt_path', default='./DataSets/ImageSets/Main', type=str, help='output txt label path')
+opt = parser.parse_args()
+
+trainval_percent = 0.9 # 训练集和验证集的比例
+train_percent = 0.9 # 训练集占总数据的比例
+imgfilepath = opt.img_path
+txtsavepath = opt.txt_path
+
+total_xml = os.listdir(imgfilepath)
+if not os.path.exists(txtsavepath):
+    os.makedirs(txtsavepath)
+
+num = len(total_xml)
+list_index = range(num)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
+# 训练集+验证集 767
+trainval = random.sample(list_index, tv)
+# 训练集 690
+train = random.sample(trainval, tr)
+# 划分生成的文件名称
+file_trainval = open(txtsavepath + '/trainval.txt', 'w')
+file_test = open(txtsavepath + '/test.txt', 'w')
+file_train = open(txtsavepath + '/train.txt', 'w')
+file_val = open(txtsavepath + '/val.txt', 'w')
+
+# 遍历range表，将训练集+验证集、训练集、验证集、测试集分别写入相应文件
+for i in list_index:
+    name = total_xml[i][:-4] + '\n'
+    if i in trainval:
+        file_trainval.write(name)
+        if i in train:
+            file_train.write(name)
+        else:
+            file_val.write(name)
+    else:
+        file_test.write(name)
+
+file_trainval.close()
+file_train.close()
+file_val.close()
+file_test.close()
+
+
+
+
